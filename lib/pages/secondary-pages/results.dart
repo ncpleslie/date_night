37bd2014b6041d307642aac7f4b15b0b87bc9dba
Nicholas Leslie <nicholasleslie@MacBook-Air.local>
2019-01-31 10:40:50 +1300

    Animations Added

--- lib/pages/secondary-pages/results.dart
+++ lib/pages/secondary-pages/results.dart
@@ -5,9 +5,18 @@ import "dart:math";
 
 import 'package:giffy_dialog/giffy_dialog.dart';
 
-class Results extends StatelessWidget {
+class Results extends StatefulWidget {
   final String result;
   Results(this.result);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ResultsState();
+  }
+}
+
+class _ResultsState extends State<Results> with SingleTickerProviderStateMixin {
   final _random = Random();
   final List<String> _gifURL = [
     "https://raw.githubusercontent.com/Shashank02051997/FancyGifDialog-Android/master/GIF's/gif14.gif",
@@ -19,41 +28,58 @@ class Results extends StatelessWidget {
     'Great Thinking, Team!',
     'Ooooooh Yeah! I Love How You Think'
   ];
+  Animation<double> _animation;
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+    _animation = CurvedAnimation(
+        parent: _animationController, curve: Curves.linear);
+    _animation.addListener(() => this.setState(() {}));
+    _animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final double _fontSizeBasedOnTextLength = result.length >= 9 ? 40.0 : 50.0;
+    final double _fontSizeBasedOnTextLength =
+        widget.result.length >= 9 ? 40.0 : 50.0;
     return Material(
       type: MaterialType.transparency,
-      child: NetworkGiffyDialog(
-        imageUrl: _gifURL[_random.nextInt(_gifURL.length)],
-        title: Text(
-          result.toUpperCase(),
-          textAlign: TextAlign.center,
-          softWrap: true,
-          style: TextStyle(
+      child: Transform.scale(
+        scale: _animation.value,
+        child: NetworkGiffyDialog(
+          imageUrl: _gifURL[_random.nextInt(_gifURL.length)],
+          title: Text(
+            widget.result.toUpperCase(),
+            textAlign: TextAlign.center,
+            softWrap: true,
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: _fontSizeBasedOnTextLength,
+                fontWeight: FontWeight.w600),
+          ),
+          description: Text(
+            _listOfAltText[_random.nextInt(_listOfAltText.length)],
+            softWrap: true,
+            textAlign: TextAlign.center,
+            style: TextStyle(
               color: Colors.white,
-              fontSize: _fontSizeBasedOnTextLength,
-              fontWeight: FontWeight.w600),
-        ),
-        description: Text(
-          _listOfAltText[_random.nextInt(_listOfAltText.length)],
-          softWrap: true,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: _fontSizeBasedOnTextLength / 2,
+              fontSize: _fontSizeBasedOnTextLength / 2,
+            ),
           ),
-        ),
-        buttonOkColor: Theme.of(context).primaryColor,
-        buttonOkText: Text(
-          'Go Do It',
-          style: TextStyle(
-            color: Colors.white,
+          buttonOkColor: Theme.of(context).primaryColor,
+          buttonOkText: Text(
+            'Go Do It',
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
+          onOkButtonPressed: () =>
+              Navigator.of(context, rootNavigator: true).pop('Continue'),
         ),
-        onOkButtonPressed: () =>
-            Navigator.of(context, rootNavigator: true).pop('Continue'),
       ),
     );
   }
