f591ff63f2469e1952efe5232447e66311767a62
Nick Leslie <nleslie@jadeworld.com>
2021-04-14 20:56:08 +1200

    Updated background

--- Client/app/lib/ui/widgets/dumb_widgets/page_background.dart
+++ Client/app/lib/ui/widgets/dumb_widgets/page_background.dart
@@ -2,6 +2,7 @@ import 'package:date_night/config/theme_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 /// The custom background that will be displayed on most screens.
 /// It can be animiated with the background fading between colors.
@@ -20,14 +21,51 @@ class PageBackground extends StatelessWidget {
     return Container(
       child: Stack(
         children: [
-          if (ThemeConfig.backgroundImage.split(".")[1] == 'svg')
-            SvgPicture.asset(
-              ThemeConfig.backgroundImage,
-              fit: BoxFit.cover,
+          PlasmaRenderer(
+            type: PlasmaType.infinity,
+            particles: 5,
+            color: Color.fromRGBO(165, 106, 184, 0.2),
+            blur: 1.0,
+            size: 1.0,
+            speed: 0.50,
+            offset: 0,
+            blendMode: BlendMode.srcOver,
+            particleType: ParticleType.circle,
+            variation1: 0,
+            variation2: 0,
+            variation3: 0,
+            rotation: 0,
+            child: PlasmaRenderer(
+              type: PlasmaType.infinity,
+              particles: 10,
+              color: Color.fromRGBO(188, 240, 254, 0.2),
+              blur: 1.0,
+              size: 1.0,
+              speed: 0.50,
+              offset: 0,
+              blendMode: BlendMode.srcOver,
+              particleType: ParticleType.circle,
+              variation1: 0,
+              variation2: 0,
+              variation3: 0,
+              rotation: 0,
             ),
-          child,
+          ),
+          child
         ],
       ),
     );
+    // return Container(
+    //   child: Stack(
+    //     children: [
+    //       if (ThemeConfig.backgroundImage.split(".")[1] == 'svg')
+    //         SvgPicture.asset(
+    //           ThemeConfig.backgroundImage,
+    //           fit: BoxFit.cover,
+    //         ),
+    //       child,
+    //     ],
+    //   ),
+    // );
   }
 }
