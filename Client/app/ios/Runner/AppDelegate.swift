e49c0d2aea504ed6b315903a7359e6ac737b008d
Nick Leslie <nleslie@jadeworld.com>
2021-04-22 21:04:54 +1200

    Hopefully fixing iOS issues

--- Client/app/ios/Runner/AppDelegate.swift
+++ Client/app/ios/Runner/AppDelegate.swift
@@ -2,15 +2,12 @@ import UIKit
 import Flutter
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, UIResponder, UIApplicationDelegate {
-
-  var window: UIWindow?
+@objc class AppDelegate: FlutterAppDelegate {
 
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
